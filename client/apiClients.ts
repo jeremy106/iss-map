526aad4fbcd155b7022a9de37342e0795a29b6df
Jeremy Webster <jeremy.webster106@gmail.com>
2024-08-05 19:20:59 +1200

    mvp coordinates

--- client/apiClients.ts
+++ client/apiClients.ts
@@ -1,10 +1,16 @@
 import request from 'superagent'
 
-export async function getCoordinates(){
-  const response = request.get("https://api.wheretheiss.at/v1/satellites/25544")
-  return response.body
+export interface Satelite {
+  name: string,
+  latitude: number,
+  longitude: number
 }
 
-
-
-
+export async function getCoordinates(){
+  const response = await request.get("https://api.wheretheiss.at/v1/satellites/25544")
+  return {
+    name: response.body.name,
+    latitude: response.body.latitude,
+    longitude: response.body.longitude
+  }
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react"
+import { getCoordinates, Satelite } from "../apiClients"
+
 function App() {
+
+  const [satelite, setSatelite] = useState<Satelite | null>(null)
+  const [error, setError ] = useState<string | null>(null)
+
+  useEffect(() => {
+    async function update() {
+      const data = await getCoordinates()
+      try {
+        setSatelite(data)
+      } catch (err) {
+        setError(String(err))
+      }
+    }
+  
+    update()
+
+  }, [])
+  
+  if (error) {
+    return <>Something went wrong: {error}</>
+  }
+
+  if (!satelite) {
+    return <>Loading...</>
+  }
+
+
   return (
-    <div>
-      <h1>ISS Point of View</h1>
-      <p>Development Underway</p>
-    </div>
+    <>
+    <h1>ISS Point of View</h1>
+    <p>
+      Latitude: {satelite.latitude}
+    </p>
+    <p>
+      Longitude: {satelite.longitude}
+    </p>
+    </>
   )
 }
 
